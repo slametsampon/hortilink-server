4ca84f0cafcf891f783ca5a99c837105aa614bb8
Slamet <slamet@pon.co.id>
2024-11-13 06:49:13 +0700

    finish setup eslint, pretier

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,22 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
+
+/* eslint-disable */
+const flowbite = require('flowbite-react/tailwind');
 
 export default {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    flowbite.content(),
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
       },
     },
   },
-  plugins: [],
+  plugins: [flowbite.plugin()],
 } satisfies Config;
